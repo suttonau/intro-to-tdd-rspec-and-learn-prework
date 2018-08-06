d6fb5365ed38bc0243531bbf3e097a344beb659a
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-08-06 13:37:52 +0000

    automatically backed up by learn

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,9 @@
+function current_age_for_birth_year(year) {
+  let currentYear = 2018
+  
+  current_age_for_birth_year = currentYear - year
+  
+  return current_age_for_birth_year
+}
+
+current_age_for_birth_year(1984)
